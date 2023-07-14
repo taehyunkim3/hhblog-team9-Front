8984c7bb321c7d46d2e2c637b9d70a7df0876f76
Taehyun Kim <agart319@gmail.com>
2023-07-14 21:37:39 +0900

    first commit

---
+++ src/pages/CreateDesk.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateDesk = () => {
+  return <div>CreateDesk</div>;
+};
+
+export default CreateDesk;
---
+++ src/pages/DeskDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeskDetail = () => {
+  return <div>DeskDetail</div>;
+};
+
+export default DeskDetail;
