87b126b6ffb9fb94a0b8ba08b93b9976a948de86
Taehyun Kim <agart319@gmail.com>
2023-07-16 17:00:16 +0900

    last commit

---
+++ src/redux/modules/desks.js
@@ -0,0 +1,8 @@
+import * as api from '../../api'
+
+// Actions
+const GET_DESKS = 'desks/GET_DESKS';
+const GET_DESKS_SUCCESS = 'desks/GET_DESKS_SUCCESS';
+const GET_DESKS_ERROR = 'desks/GET_DESKS_ERROR';
+
+const POST_DESK = 'desks/POST_DESK';
